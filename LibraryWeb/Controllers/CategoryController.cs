bc6eced9e95b50e1a25dc2989d2d61779703e1d3
k3rnel_panic <shotov1303@gmail.com>
2023-06-04 23:58:45 +0400

    Added Bootswatch Custom Theme

---
+++ LibraryWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using LibraryWeb.Data;
+using LibraryWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+
+
+    }
+}
